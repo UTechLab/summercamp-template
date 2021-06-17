0504fc8cd1a71d4ea914dc00cfc5aa101fdf8ccb
UTechLab <86042871+UTechLab@users.noreply.github.com>
2021-06-17 11:08:16 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+scene.setBackgroundColor(0)
+tiles.setTilemap(tilemap`level1`)
+let MyPlayer = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(MyPlayer, tiles.getTileLocation(0, 0))
